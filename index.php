09c9319a8221d62d983990653c90012acd1c1551
Zlatin Hristov <zlatin.hristov@softavis.com>
2023-05-10 00:29:50 +0300

    Change Symfony with Slim

--- index.php
+++ index.php
@@ -1,9 +1,48 @@
 <?php
 
-use App\Kernel;
+declare(strict_types=1);
 
-require_once 'vendor/autoload_runtime.php';
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+require __DIR__.'/vendor/autoload.php';
+
+$app = AppFactory::create();
+$app->setBasePath($_SERVER['BASE'] ?? '');
+
+$app->any('/[{path:.*}]', function (Request $request, Response $response, array $args) {
+
+    if (!isset($args['path']) || !strchr($args['path'], 'api')) {
+        $response->getBody()->write('Change URL!');
+
+        return $response;
+    }
+
+    $host = 'https://api-studentski.bidlag.com/';
+
+    $headers = [];
+    foreach ($request->getHeaders() AS $headerName => $headerValue) {
+        $headers[] = $headerName.': '.implode(', ', $headerValue);
+    }
+
+    $curl = curl_init($host.$args['path']);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+    if (in_array($request->getMethod(), ['POST', 'PUT'], true)) {
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $request->getParsedBody());
+    }
+
+
+    $data = curl_exec($curl);
+
+    curl_close($curl);
+
+    $response->getBody()->write($data);
+
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->run();
